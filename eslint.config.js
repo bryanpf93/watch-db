bb4099af5e6feebd39d6c8559b2ec37396e227e2
Joel Paucarima Franco <joel.dev.3112@gmail.com>
2026-04-29 15:36:29 +0200

    chore: add prettier+eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,39 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
+
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
+
+    plugins: {
+      "simple-import-sort": simpleImportSort
+    },
+
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier
     ],
-    languageOptions: {
-      globals: globals.browser,
+
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      // no console
+      "no-console": "error"
     },
-  },
-])
+
+    languageOptions: {
+      globals: globals.browser
+    }
+  }
+]);
